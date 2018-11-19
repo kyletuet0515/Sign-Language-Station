c26196c2faee2a1fea13b876eaae2eec67041363
kyletuet0515 <45030818+kyletuet0515@users.noreply.github.com>
2018-11-19 22:42:17 +0800

    i dont have permission so i pull request
    just filling the content. part of it come from background in midterm report which fits the requirement of aboutUs

--- aboutUs.php
+++ aboutUs.php
@@ -22,7 +22,22 @@ $(document).ready(function(){
 	<?php require('header.php');?>
 	
 	<div class="mainFrame">
-		wait to be finished...
+		Rationale of station:<br><br>
+		With the ever accelerated development of public welfare and social benefit, more and more people are paying attention to the 
+		living conditions of the disabled. The very first obstacle for deaf people to have a normal life is the disability to communicate
+		with others, there is no doubt that the generalization of sign language is of great importance in improving their living conditions.<br><br>
+                However, currently we are lacking an online platform for the public to access to the education of sign language even though they want to learn.
+		There are few self-learning accesses whereas our society is lacking members able to communicate with the deaf. 
+		The traditional way of training is of low efficiency thus fails to fulfill the actual need. 
+		That’s why we are convinced that a self learning tool for sign language is in demand.<br><br>
+        
+		How sign in Hong Kong come from?<br>
+		As sign language are uncommon, there is a lack of of sign language. Also, sign language changes as time pass.
+		School may change some of the usual sign language for teaching purpose.<br><br>
+		
+		Purpose of station:<br>
+		*Improve sign language dictionary to help deaf people<br>
+		*Promote sign language in Hong Kong<br>
 	</div>s
 	
 	<?php require('footer.php');?>
